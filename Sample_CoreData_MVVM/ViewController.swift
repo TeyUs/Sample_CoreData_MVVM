c5ad2c433259e2f2468f52cf88b1652e832c028a
Teyhan USLU <teyhan.uslu@accenture.com>
2022-05-09 22:31:12 +0300

    Initial Commit

---
+++ Sample_CoreData_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample_CoreData_MVVM
+//
+//  Created by teyhan.uslu on 9.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
